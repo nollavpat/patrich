f343ba670d811edffe73c28459d8ca7b3d66a6f2
Patrick Vallon <jpatrickvallon@gmail.com>
2022-06-01 20:47:16 +0800

    Init

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'components/expandable_fab/expandable_fab.dart';
+import 'components/expandable_fab/action_button.dart';
+import 'components/home/home.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: App(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  static const _actionTitles = ['Create Post', 'Upload Photo', 'Upload Video'];
+
+  const App({super.key});
+
+  void _showAction(BuildContext context, int index) {
+    showDialog<void>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Text(_actionTitles[index]),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('CLOSE'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Patrich'),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        itemCount: 25,
+        itemBuilder: (context, index) {
+          return Home(isBig: index.isOdd);
+        },
+      ),
+      floatingActionButton: ExpandableFab(
+        distance: 112.0,
+        children: [
+          ActionButton(
+            onPressed: () => _showAction(context, 0),
+            icon: const Icon(Icons.format_size),
+          ),
+          ActionButton(
+            onPressed: () => _showAction(context, 1),
+            icon: const Icon(Icons.insert_photo),
+          ),
+          ActionButton(
+            onPressed: () => _showAction(context, 2),
+            icon: const Icon(Icons.videocam),
+          ),
+        ],
+      ),
+    );
+  }
+}
